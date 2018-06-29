eb6466ca9dfb34e0c48bc97dc15d5d3b3d874ff9
Daniel Strini <dstrini@incognito.com>
2018-06-28 18:58:54 -0700

    testing PyCharm

--- first_project/hello.py
+++ first_project/hello.py
@@ -8,7 +8,12 @@ print("Hello")
 r = requests.get('http://www.hobbes.nmsu.edu')
 print(r)
 var = uplink.dumps
-for _ in range(1,10):
+for _ in range(1, 10):
     time.sleep(0.4)
     print('.', end='')
     sys.stdout.flush()
+    print()
+
+
+def add_test():
+    pass
